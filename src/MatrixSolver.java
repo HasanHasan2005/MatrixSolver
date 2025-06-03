3d0e24759a2c654fe90c37f212073e5500d25994
HasanHasanUCD <hasan.hasan@ucdconnect.ie>
2025-06-04 00:05:40 +0100

    added RREF calculator

--- src/MatrixSolver.java
+++ src/MatrixSolver.java
@@ -4,11 +4,20 @@ public class MatrixSolver {
 
     public static void main(String[] args) {
 
-        Fraction[][] data = {
-                {new Fraction(2), new Fraction(3), new Fraction(0)},
-                {new Fraction(6), new Fraction(1), new Fraction(-6)},
-                {new Fraction(5), new Fraction(5), new Fraction(0)}};
+        int precision = 3;
 
+        Matrix m1 = new Matrix(new double[][]{
+                {0.022, 0.033, 23.44, 607.87},
+                {51.22, 81.09, 5.6007, 1.8902},
+                {6.667, 3.446, 4.555, 3.001}}, precision);
 
+        Matrix m2 = new Matrix(new int[][]{
+                {1, 8},
+                {2, 7},
+                {3, 6},
+                {4, 5}});
+
+        m1.multiply(m2);
+        m1.printAsDecimal();
     }
 }
